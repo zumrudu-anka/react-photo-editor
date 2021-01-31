b32592c2e79fa681d5f1931bc2f0d4eb8a23d26d
Zümrüd-ü Anka <osmandurdag@hotmail.com>
2021-02-01 00:17:57 +0300

    Updates

---
+++ precache-manifest.4d9b7ac95c8d0106b55d50dd92245896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1f7f3a73354323a57db072bcb95dcc",
+    "url": "/react-photo-editor/index.html"
+  },
+  {
+    "revision": "af520a9a2eba6b7a3aba",
+    "url": "/react-photo-editor/static/css/main.0e3ecd0b.chunk.css"
+  },
+  {
+    "revision": "eb9256d8ef6980b70a24",
+    "url": "/react-photo-editor/static/js/2.26d0de0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-photo-editor/static/js/2.26d0de0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af520a9a2eba6b7a3aba",
+    "url": "/react-photo-editor/static/js/main.5e3ac721.chunk.js"
+  },
+  {
+    "revision": "fbd9264325052025f4d5",
+    "url": "/react-photo-editor/static/js/runtime-main.90bd41ce.js"
+  }
+]);
